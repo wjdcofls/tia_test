90df4960ad5c132eca0f1a52aeba02712926a3ac
Jeong ChaeLin(정채린) <a01064445253@gmail.com>
2023-11-29 17:30:53 +0900

    [UPDATE] 과제 10까지
    (23.11.29. 화 17:30)

--- app/src/main/java/com/example/testapplication/edit_text.java
+++ app/src/main/java/com/example/testapplication/edit_text.java
@@ -2,11 +2,17 @@ package com.example.testapplication;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
@@ -22,12 +28,18 @@ public class edit_text extends AppCompatActivity {
         }
     }
 
+    private boolean isPasswordVisible = false;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_text);
 
         Button btn_3 = findViewById(R.id.btn3);
+        EditText et2 = findViewById(R.id.et2);
+        ImageView iconEyeOpen = findViewById(R.id.icon_eye_open);
+        ImageView iconEyeClose = findViewById(R.id.icon_eye_closed);
+
 
         btn_3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,5 +60,65 @@ public class edit_text extends AppCompatActivity {
             }
         });
 
+
+        //Logcat
+        et2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count){}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                String inputValue = charSequence.toString();
+                Log.d("input_test", "Keyboard Input: " + inputValue);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable){}
+        });
+
+
+        // eye-watching
+        et2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+                if (charSequence.length() > 0) {
+                    iconEyeOpen.setVisibility(View.VISIBLE);
+                } else {
+                    iconEyeOpen.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+
+        iconEyeOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 마스킹 해제
+                et2.setTransformationMethod(null);
+                isPasswordVisible = true;
+                iconEyeOpen.setVisibility(View.INVISIBLE);
+                iconEyeClose.setVisibility(View.VISIBLE);
+                et2.setSelection(et2.length());
+            }
+        });
+
+        iconEyeClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 다시 마스킹 처리
+                et2.setTransformationMethod(new PasswordTransformationMethod());
+                isPasswordVisible = false;
+                iconEyeOpen.setVisibility(View.VISIBLE);
+                iconEyeClose.setVisibility(View.INVISIBLE);
+                et2.setSelection(et2.length());
+            }
+        });
     }
 }
